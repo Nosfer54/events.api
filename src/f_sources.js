37e786c273e13e3f2bc1fd64b9ffe7abb526b7d7
Alexandr Kharin <alexandr.kharin@skbkontur.ru>
2018-08-31 18:54:32 +0700

    выводим список событийв документ

--- src/f_sources.js
+++ src/f_sources.js
@@ -1,14 +1,14 @@
 var axios         = require('axios');
 
-const auth          = require( '../conf.js');
+//const auth        = require( '../conf.js');
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 axios.get('http://localhost:1337/api', // url
     {
-        headers: {
-            'Authorization': auth,
-        },
+  //      headers: {
+    //        'Authorization': auth,
+      //  },
         // `withCredentials` indicates whether or not cross-site Access-Control requests
         // should be made using credentials
         withCredentials: true // Может быть надо, а может быть нет
@@ -16,6 +16,11 @@ axios.get('http://localhost:1337/api', // url
 )
     .then(function (response) {
         console.log(response.data);
+        for (var i = 0; i < response.data.items.length; i++)
+        {
+            document.getElementById("min").innerHTML = document.getElementById("min").innerHTML + response.data.items[i]['source-id'] + "<br>";
+        }
+
     })
     .catch(function (error) {
         console.log(error);
